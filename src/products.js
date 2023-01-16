95409fd6141cd5cf4c50a530eb9a9b189f8c5e77
Kshitij <kshitijk20@iitk.ac.in>
2023-01-16 12:39:12 +0530

    first commit

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/mac-1.jpg";
+import product2 from "./assets/products/iphone.jpg";
+import product3 from "./assets/products/canon.jpg";
+import product4 from "./assets/products/Denim.jpg";
+import product5 from "./assets/products/wallet.jpg";
+import product6 from "./assets/products/black.jpg";
+import product7 from "./assets/products/hat.jpg";
+import product8 from "./assets/products/jacket.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "MacBook Air",
+    price: 119000.00,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Iphone 12",
+    price: 60890.00,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Canon EOS Camera",
+    price: 35988.00,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Men's Jeans",
+    price: 999.00,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Men's Wallet",
+    price: 799.00,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Adidas Shoes",
+    price: 1458.00,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Woelen Hat",
+    price: 636.00,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Denim Jacket",
+    price: 3900.00,
+    productImage: product8,
+  }
+];
